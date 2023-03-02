0ec2d5eb9fb99c1fd18c1178e5e2641ba8b76a69
krishnachaitanya7 <chaitanyaradon89@gmail.com>
2023-03-01 17:58:06 -0800

    sync

--- scripts/ridgeback_scripts/ridgeback_set_goal.py
+++ scripts/ridgeback_scripts/ridgeback_set_goal.py
@@ -7,14 +7,14 @@ def main():
     pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
     rospy.sleep(1)
     goal = PoseStamped()
-    goal.header.frame_id = "map"
-    goal.pose.position.x = -0.033
-    goal.pose.position.y = 1.544
+    goal.header.frame_id = "odom"
+    goal.pose.position.x = -0.4
+    goal.pose.position.y = 3.9
     goal.pose.position.z = 0.0
     goal.pose.orientation.x = 0.0
     goal.pose.orientation.y = 0.0
-    goal.pose.orientation.z = 0.462
-    goal.pose.orientation.w = 0.886
+    goal.pose.orientation.z = -0.619
+    goal.pose.orientation.w = 0.786
     pub.publish(goal)
 
 
